bdbb63f89fdd780c9df2dd1394f1cbdbe41bd6bd
Gabriel Braga <gabrielzinhobraga04@gmail.com>
2023-05-11 14:43:41 -0300

    DataBase seed update

--- src/main/java/com/devSup/dslist/entities/GameList.java
+++ src/main/java/com/devSup/dslist/entities/GameList.java
@@ -8,7 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 @Entity
-@Table(name = "tb_gamelist")
+@Table(name = "tb_game_list")
 public class GameList {
 	
 	@Id
